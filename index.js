fc3faeb99f25e400790d58233fbf1a695e3c1965
sandeep14032004 <sandeeppati69@gmail.com>
2025-03-16 11:12:30 +0530

    update frontend

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ export const instance = new Razorpay({
 
 const app = express();
 
-//using middlewares
 app.use(express.json());
 app.use(cors());
 
@@ -22,12 +21,10 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 app.use("/uploads", express.static("uploads"));
-//importing routes
 import userRoutes from "./routes/user.js";
 import courseRoutes from "./routes/course.js";
 import adminRoutes from "./routes/admin.js";
 
-//using routes
 app.use("/api", userRoutes);
 app.use("/api", courseRoutes);
 app.use("/api", adminRoutes);
